bf205633faeb50f53be77f7d227c3a6b7ea22b72
Adnan Chahbandar <me@adnan.io>
2025-05-21 21:41:04 -0500

    feat: added coming soon component

---
+++ app/components/ComingSoon.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="min-h-screen flex items-center justify-center">
+      <div class="text-center">
+        <slot>
+          <h2 class="my-4">Ladybug Lakeview:</h2>
+          <p class="text-xl">Coming Soon!</p>
+        </slot>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CenteredText'
+  }
+  </script>
--- app/layouts/home.vue
+++ app/layouts/home.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="px-10 py-20 text-center">
     <slot />
-    <Footer />
-    <div class="text-sm mx-auto mt-5 text-center opacity-25">
+    <!-- <Footer /> -->
+    <!-- <div class="text-sm mx-auto mt-5 text-center opacity-25">
       [Home Layout]
-    </div>
+    </div> -->
   </main>
 </template>
